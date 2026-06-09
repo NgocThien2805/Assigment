e58ec40c073e22c2382508ecea0d80b794280e6b
way205-code <buihathieb20052007@gmail.com>
2026-06-09 20:13:09 +0700

    Create thien

--- thien.java
+++ thien.java
@@ -10,16 +10,16 @@ public class thien {
         int n1 = 0;
         int n2 = 1;
         int n3 = 0;
-        while (n3 < n) {
+        while (n>n1) {
             System.out.println(n1 + " ");
             n3 = n1+n2; 
             n1 = n2;
             n2 = n3;
-           
 
-            
+    
+
+
         }
-    System.out.println(n3);
 }
     
 }
