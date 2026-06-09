80b1a65b0f125226a72439f655fffaeb4f5d85aa
way205-code <buihathieb20052007@gmail.com>
2026-06-09 19:50:46 +0700

    Create thien

---
+++ thien.java
@@ -0,0 +1,25 @@
+package Assigment;
+
+import java.util.Scanner;
+
+public class thien {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("nhap so ");
+        int n = sc.nextInt();
+        int n1 = 0;
+        int n2 = 1;
+        int n3 = 0;
+        while (n3 < n) {
+            System.out.println(n1 + " ");
+            n3 = n1+n2; 
+            n1 = n2;
+            n2 = n3;
+           
+
+            
+        }
+    System.out.println(n3);
+}
+    
+}
